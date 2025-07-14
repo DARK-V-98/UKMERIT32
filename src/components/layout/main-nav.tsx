3e856bbf99a265a3a5df5a6d29ca49683347ee35
Thimira Vishwa <vishwathimira2@gmail.com>
2025-07-14 05:02:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -43,14 +43,9 @@ export function MainNav() {
         {/* Mobile Nav */}
         <div className="md:hidden">
             <Sheet>
-              <SheetTrigger asChild>
-                <Button
-                  variant="ghost"
-                  className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"
-                >
-                  <Menu className="h-6 w-6" />
-                  <span className="sr-only">Toggle Menu</span>
-                </Button>
+              <SheetTrigger className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden">
+                <Menu className="h-6 w-6" />
+                <span className="sr-only">Toggle Menu</span>
               </SheetTrigger>
               <SheetContent side="left" className="pr-0">
                  <Link
